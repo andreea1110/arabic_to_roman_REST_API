f3176c1db155aa4e5b86489a3225a77d75e15de9
Andreea Ciuprina <andreea.ciuprina@alumni.ethz.ch>
2021-08-16 15:48:07 +0200

    Added test class for main invocation

--- src/test/java/com/adobe/interview/InterviewApplicationTests.java
+++ src/test/java/com/adobe/interview/InterviewApplicationTests.java
@@ -3,11 +3,13 @@ package com.adobe.interview;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// Test class added ONLY to cover main() invocation not covered by application tests.
 @SpringBootTest
 class InterviewApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	public void main() {
+		InterviewApplication.main(new String[] {});
+	 }
 
 }
