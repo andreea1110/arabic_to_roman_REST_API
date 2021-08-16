6c6e6b88d589ff2420cf31c2f654e6a619098ada
Andreea Ciuprina <andreea.ciuprina@alumni.ethz.ch>
2021-08-16 15:48:34 +0200

    Add integration test for RomanNumeralsController

---
+++ src/test/java/com/adobe/interview/RomanNumeralsControllerTest.java
@@ -0,0 +1,63 @@
+package com.adobe.interview;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.server.ResponseStatusException;
+
+@SpringBootTest(
+    classes = InterviewApplication.class,
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class RomanNumeralsControllerTest {
+
+    @LocalServerPort //autowire random port into the port class private variable
+	private int port;
+
+	TestRestTemplate restTemplate = new TestRestTemplate();
+
+	HttpHeaders headers = new HttpHeaders();
+
+    private String createURLWithPort(String uri) {
+		return "http://localhost:" + port + uri;
+	}
+
+
+    // testing error status code (400 Bad Request if range outside set boundaries)
+    @Test
+    public void testConvertNumbers_outsideBoundaries__400IsReceived() throws ResponseStatusException {
+
+        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+		ResponseEntity<String> response = restTemplate.exchange(createURLWithPort("/arabicToRoman?min=1&max=105"),
+                                                                HttpMethod.GET, 
+                                                                entity,
+                                                                String.class);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+
+    // testing JSON payload
+	@Test
+    public void testConvertNumbers_withinBoundaries_retrievedJSONisCorrect() {
+
+		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+		ResponseEntity<String> response = restTemplate.exchange(createURLWithPort("/arabicToRoman?min=1&max=10"),
+                                                                HttpMethod.GET, 
+                                                                entity,
+                                                                String.class);
+
+		String expected = "{\"id\":1,\"content\":[\"I\",\"II\",\"III\",\"IV\",\"V\",\"VI\",\"VII\",\"VIII\",\"IX\",\"X\"]}";
+
+		assertEquals(expected, response.getBody());
+	}
+}   
