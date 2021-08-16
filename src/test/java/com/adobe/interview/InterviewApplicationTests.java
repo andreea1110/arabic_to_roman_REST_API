3156096d93d0855d07201aa3b82d7d0481ba7972
Andreea Ciuprina <andreea.ciuprina@alumni.ethz.ch>
2021-08-16 16:01:15 +0200

    remove test class for main

--- src/test/java/com/adobe/interview/InterviewApplicationTests.java
+++ src/test/java/com/adobe/interview/InterviewApplicationTests.java
@@ -3,13 +3,11 @@ package com.adobe.interview;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// Test class added ONLY to cover main() invocation not covered by application tests.
 @SpringBootTest
 class InterviewApplicationTests {
 
 	@Test
-	public void main() {
-		InterviewApplication.main(new String[] {});
-	 }
+	void contextLoads() {
+	}
 
 }
